123dc8f95b59bbdf423bb5de3ec6a75509b9875f
kennysalam <kennys.salam@gmail.com>
2020-01-10 13:42:35 +0700

    user dan weather client

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
-
+    
 })
+function switchToHome(){
+    $("#login").hide()
+    $("#detail").hide()
+    $("#landing").hide()
+    $("#home").show()
+}
+
+function switchToDetail() {
+    $("#login").hide()
+    $("#home").hide()
+    $("#landing").hide()
+    $("#detail").show()
+}
+
+function switchToLogin() {
+    $("#home").hide()
+    $("#detail").hide()
+    $("#landing").hide()
+    $("#login").show()
+}
+
+function switchToLanding() {
+    $("#login").hide()
+    $("#home").hide()
+    $("#detail").hide()
+    $("#landing").show()
+}
